ca29c66a98f75e13cb37d765d0193fd31db9fdbf
David <locke.David@student.greenriver.edu>
2024-05-01 11:16:08 -0700

    week 5

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 # Script that encrypts/decrypts text using ROT13
-# By 
+# By David Locke 4/30
+
+#get message
+message = input("what is the message? ")
+#for each letter
+for letter in message:
+    print (letter)
+    #rotate 13 letters
+        #change to number
+        #add 13
+        #GTR than 26?
+            #sub 26
+        #change to letter
+#print message
