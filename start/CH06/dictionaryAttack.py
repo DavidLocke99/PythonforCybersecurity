944c1b03f71e75b9975c314f536d1b5a81b659eb
David <locke.David@student.greenriver.edu>
2024-05-07 09:53:09 -0700

    week6

--- start/CH06/dictionaryAttack.py
+++ start/CH06/dictionaryAttack.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 # Script that performs a dictionary attack against known password hashes
 # Needs a dictionary file, suggested to use https://github.com/danielmiessler/SecLists/tree/master/Passwords/Common-Credentials
-# By 
+# By 5/6 David
+
+import crypt
+import os
+
+file_path=os.path.dirpath(__file__)
+print (f"The path of the script is {file_path}")
+
+# ask for ID and Salt
+id_salt = input("what is the ID and Salt?")
+# ask for fully salted hash
+salted_pass = input("what is the fully salted and hash") 
+# open pw file
+f=open("top10.txt","r")
+guesses=f.read()
+print(guesses)
+# for each guess in pw file
+    # hash the guess
+    # is guess = to target?
+        # print guess then quit
