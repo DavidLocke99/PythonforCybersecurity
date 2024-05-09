254039b27b737bb874c0a9a8bafb45d88d1f1ea0
David <locke.David@student.greenriver.edu>
2024-05-09 09:08:39 -0700

    weeks6b

--- start/CH06/dictionaryAttack.py
+++ start/CH06/dictionaryAttack.py
@@ -11,18 +11,18 @@ file_path=os.path.dirname(__file__)
 # ask for ID and Salt
 id_salt = input("what is the ID and Salt?")
 # ask for fully salted hash
-salted_pass = input("what is the fully salted and hash") 
+salted_pass = input("what is the fully salted and hash ") 
 # open pw file
-f=open(file_path+ "/top10.txt","r")
+f=open(file_path+ "/10-million-password-list-top-1000000.txt","r")
 # for each guess in pass file
 for guess in f:
     guess=guess.strip()
 
     # hash the guess
-    hashedg_guess=crypt.crypt(guess,id_salt)
+    hashed_guess=crypt.crypt(guess,id_salt)
     
     # is guess = to target?
-    if hashedg_guess==salted_pass:
+    if hashed_guess==salted_pass:
         # print guess and quit
         print(guess)
         exit()
