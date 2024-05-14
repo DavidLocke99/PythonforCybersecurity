4c8513cdb5ab7ae885620ef65cd9e23332cb51c1
DavidLocke99 <91079115+DavidLocke99@users.noreply.github.com>
2024-05-14 08:55:30 -0700

    Update Find404.py

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
-# Script that scans web server logs for 404 errors
-# By 
+# By 5/14 David Locke
+
+import os
+
+file_path=os.path.dirname(__file__)
+
+# prompt for filename
+log_file=input("which file to analyze? ")
+#open file
+f=open(file_path+"/"+log_file, "r")
+
+#read file line by line
+while True:
+    line=f.readline()
+    if not line:
+        break
+    #check for 404
+    if " 404 " in line:
+        #print line
+        print(line)
