030633502a04ec79679f1447bcc660e847a6c391
David <locke.David@student.greenriver.edu>
2024-05-28 09:48:57 -0700

    building

--- start/CH08/HaveIBeenPwned.py
+++ start/CH08/HaveIBeenPwned.py
@@ -3,35 +3,38 @@
 # https://haveibeenpwned.com/API/v3#PwnedPasswords
 # By 5/28 david
 
-#what we know
-    # how the api works 
 import requests
+import hashlib
 
-url = "https://api.pwnedpasswords.com/range/range/b1b27"
-
-payload={}
-headers = {}
-
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
 # function for haveibeenpwned
+
+
+def check_password(first_five):
+    url = "https://api.pwnedpasswords.com/range/"+first_five
+    payload={}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.text
 
 
 # ask for passwword
 
-
+pass_to_test="qwerty"
 # generate sha-1 hash
 
 
-# call api
+def SHA1(msg:str) -> str:
+        encoded_string = msg.encode
+        hash_result =hashlib.sha1(msg.encode()).hexdigest()
+        return hash_result.hexdigest()
 
+# ask for PW
+pass_to_test="qwerty"
 
-# search though results
+# get hash and split
 
+hash_full = SHA1("qwerty")
+hash_start=hash_full[0:5]
+hash_end=hash_full[5:]
 
-# report found or not found
-
-
-# what we don't know
-    # how to split strings into 2 parts
