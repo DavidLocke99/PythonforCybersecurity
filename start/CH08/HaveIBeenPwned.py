2d0a3e79c9a9743eddeeba8ecdf507949ac0a5f6
David <locke.David@student.greenriver.edu>
2024-05-28 08:58:25 -0700

    building code

--- start/CH08/HaveIBeenPwned.py
+++ start/CH08/HaveIBeenPwned.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
 # Script that checks passwords agains haveibeenpwned.com API
 # https://haveibeenpwned.com/API/v3#PwnedPasswords
-# By 
+# By 5/28 david
+
+#what we know
+    # how the api works 
+import requests
+
+url = "https://api.pwnedpasswords.com/range/range/b1b27"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+# function for haveibeenpwned
+
+
+# ask for passwword
+
+
+# generate sha-1 hash
+
+
+# call api
+
+
+# search though results
+
+
+# report found or not found
+
+
+# what we don't know
+    # how to split strings into 2 parts
