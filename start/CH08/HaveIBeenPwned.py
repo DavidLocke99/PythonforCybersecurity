a6807ab1257b55d5a24cbd8e5108e4441ceb85d7
David <locke.David@student.greenriver.edu>
2024-05-28 13:41:08 -0700

    building

--- start/CH08/HaveIBeenPwned.py
+++ start/CH08/HaveIBeenPwned.py
@@ -17,24 +17,25 @@ def check_password(first_five):
 
     return response.text
 
-
 # ask for passwword
 
 pass_to_test="qwerty"
-# generate sha-1 hash
 
-
+# generate sha-1 hex digets for some input string
 def SHA1(msg:str) -> str:
-        encoded_string = msg.encode
-        hash_result =hashlib.sha1(msg.encode()).hexdigest()
-        return hash_result.hexdigest()
+    encoded_string = msg.encode()
+    hash_result = hashlib.sha1(encoded_string)
+    return hash_result.hexdigest()
 
 # ask for PW
 pass_to_test="qwerty"
-
 # get hash and split
-
 hash_full = SHA1("qwerty")
 hash_start=hash_full[0:5]
 hash_end=hash_full[5:]
 
+#call api
+pwned_results = check_password(hash_start)
+
+#search through results
+print(pwned_results)
