009db220fb463b5d428b59bd7220eb7c7048c233
David <locke.David@student.greenriver.edu>
2024-06-04 08:38:40 -0700

    garbage

--- start/CH09/ScanFile.py
+++ start/CH09/ScanFile.py
@@ -3,8 +3,31 @@
 # https://developers.virustotal.com/reference
 # By 
 
-#file
+# file
 
-#hash of file
+# hash file
+# get file report
+# upload file
+# get api key
+def get_api_key(key_name)
+    config=configparser.ConfigParser()
+    config.read('/home/justincase/secrrets.ini')
+    api_key=config["APIKeys"][key_name]
+    return api_key
 
-#api key
+#ask for file name
+# hash the file
+# get api report
+def get_vt_report(api_key, file_hash):
+    import requests
+    payload={}
+    headers={
+    'x-apikey':api_key
+    }
+
+# has this been seen?
+    #if yes, print reuslts and quit
+#upload file to api
+    #loop 5 times
+    #wait xxx seconds   
+    #
